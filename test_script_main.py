1f030f803eb1e49ce00b7106af7a4fc979d2a5b3
ktchery <ktchery@users.noreply.github.com>
2023-12-05 11:08:35 -0500

    setting headless to true

--- test_script_main.py
+++ test_script_main.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 @pytest.fixture(scope="session")
 def playwright_browser():
     with sync_playwright() as playwright:
-        browser = playwright.firefox.launch(headless=False, slow_mo=1000)
+        browser = playwright.firefox.launch(headless=True, slow_mo=1000)
         yield browser
         browser.close()
 
